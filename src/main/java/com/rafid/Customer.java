41293038d6147adbe4714f113315997322ba3319
rafid-tawhid <rafidtawhid@gmail.com>
2025-02-02 23:57:27 +0600

    update

--- src/main/java/com/rafid/Customer.java
+++ src/main/java/com/rafid/Customer.java
@@ -4,17 +4,17 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
-//@Entity
+@Entity
 public class Customer {
-//    @Id
-//    @SequenceGenerator(
-//            name = "customer_id_sequence",
-//            sequenceName = "customer_id_sequence"
-//    )
-//    @GeneratedValue(
-//            strategy = GenerationType.SEQUENCE,
-//            generator ="customer_id_sequence"
-//    )
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator ="customer_id_sequence"
+    )
     private  Integer id;
     private String name;
     private String email;
