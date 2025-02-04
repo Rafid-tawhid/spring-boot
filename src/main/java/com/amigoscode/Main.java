a24a45fecf7a1029c5410491fc2dd7ec032e1b23
rafid-tawhid <rafidtawhid@gmail.com>
2025-02-05 00:35:29 +0600

    update

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,18 +1,22 @@
 package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
 @RestController
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.amigoscode")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         System.out.println("Hello World!");
     }
-    @GetMapping("/")
+    @GetMapping("/greet")
     public  String greet(String name) {
         return "Hello " + name + "!";
     }
