cfec7e19ca1db32fa91a7cc04238d009dfb4da66
Rafid-tawhid <rafidtawhid@gmail.com>
2025-02-06 23:33:11 +0600

    update

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//
+
 //@SpringBootApplication
 @RestController
 @EnableAutoConfiguration
